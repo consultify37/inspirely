65bfd2a281c0cecb06086d6097f19928af8b9412
Andrei Radu <andrei1.radu93@gmail.com>
2024-10-01 10:57:03 +0300

    .

--- utils/FormatText2.tsx
+++ utils/FormatText2.tsx
@@ -10,7 +10,31 @@ const FormatText2 = ({ text, className }: Props) => {
     <>
       {
         text.split('\n').map((item, index) => (
-            <p className={className} key={index}>{item}</p>
+            <p className={className} key={index}>{
+              item.split('*').map((item1, index) => (
+                <span key={item1+index}>
+                    { index == 1 ? 
+                      <span className="font-bold">
+                        {
+                          item1.split('/').map((item2, index) => (
+                            <span key={item2+index}>
+                                {index == 1 ? <span className="italic">{item2}</span> : <span>{item2}</span>}
+                            </span>
+                          ))
+                        }
+                      </span> : 
+                      <span>
+                        {
+                          item1.split('/').map((item2, index) => (
+                            <span key={item2+index}>
+                                {index == 1 ? <span className="italic">{item2}</span> : <span>{item2}</span>}
+                            </span>
+                          ))
+                        }
+                      </span>}
+                </span>
+            ))
+            }</p>
         )) 
       }
     </>
